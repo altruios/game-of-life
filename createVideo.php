b98492ba8dc0240c369f56b182c6d9d5560e0ec1
altruios <49083558+altruios@users.noreply.github.com>
2020-03-07 00:46:33 -0800

    render function now avalible if on a server

---
+++ createVideo.php
@@ -0,0 +1,10 @@
+
+<?php
+//is this right?
+// must have ffmpeg installed and in the main c or set to a global
+shell_exe('C:\xampp\htdocs\gameOfLife\images>ffmpeg -r 19 -f image2  -i image_%d.png outputfile.mov');
+
+
+
+
+?>
