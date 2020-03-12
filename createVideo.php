c2bea20001b79f3a1000c0a03b51daecf385940b
altruios <49083558+altruios@users.noreply.github.com>
2020-03-12 12:43:00 -0700

    more finished
    not yet public facing ready, but if you know javascript,
    pretty sure you can see what's happening and set it up on your own server...
    default settings for resolution are for testing.
    color clock is a new feature - still working on getting that working how i want.

--- createVideo.php
+++ createVideo.php
@@ -2,9 +2,29 @@
 <?php
 //is this right?
 // must have ffmpeg installed and in the main c or set to a global
-shell_exe('C:\xampp\htdocs\gameOfLife\images>ffmpeg -r 19 -f image2  -i image_%d.png outputfile.mov');
 
+$dateString = strtotime("now");
+var_dump(shell_exec("which ffmpeg"));
+$cmd = "C:/xampp/htdocs/gameOfLife/images/ffmpeg -r 19 -f image2  -i image_%d.png output_t3.mov";
+$output = [];
+$didItRun=0;
+exec($cmd, $output, $didItRun);
+print("did it run? ".$didItRun);
+print("did it run? <br>");
+for($i=0;$i<sizeOf($output);$i++)
+	{
+	print("just testing");	
+	print("output? ".$output[$i]);
+}
 
+exec("C:/xampp/htdocs/gameOfLife/images/image_1.png",$output, $didItRun);
 
+print("did this run? ".$didItRun);
+for($i=0;$i<sizeOf($output);$i++)
+	{
+	print("just testing");	
+	print("output? ".$output[$i]);
+}
+print("did it run? <br>");
 
 ?>
